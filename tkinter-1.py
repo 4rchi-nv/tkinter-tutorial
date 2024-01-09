9eae097699a227348aff2b925c1e4e5d9734ac58
Archie <agajanov0arslan@gmail.com>
2024-01-09 15:42:39 +0500

    git commit perevod

--- tkinter-1.py
+++ tkinter-1.py
@@ -2,10 +2,14 @@
 from tkinter import *
 
 #calling Tk() method
+#вызываем Tk метод
 root = Tk() 
 #its used for creating gui window 
+# gui - graphic user interface
+
 
 #title() method is used to change the title 
+#title() используется для изменения заголовка 
 root.title("Zagolovok")
 
 #geometry() method is used to resize the Gui Window
